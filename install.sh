7178ab5eaff10b77f41ef39801c1a222bb8d741e
Andrew <andrew.rattana@gmail.com>
2014-09-12 00:56:04 -0400

    script updates

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo
+
+DIR=~/dotfiles
+FILES=".bash_aliases .inputrc"
+
+# echo "Creating dotfiles folder at Root..."
+# mkdir -v $DIR
+# echo "done!"
+
+# echo -n "Going into dotfiles"
+# cd $DIR
+# echo $PWD
+
+# if [ ! -a ~/.inputrc ]; then 
+#   echo "\$include /etc/inputrc" > ~/.inputrc
+# fi
+
+# Symlink Files
+# for file in $FILES; do
+#   echo -n "Symlinking dotfile... "
+#   ln -sv $DIR/$file ~/$file
+#   echo "Done!"
+# done
+
+# echo
+
+# Install Apps
+# Chrome / XAMP / RVM / Sublime / 
+
+
+
+
