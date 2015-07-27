baf1e2da56fee6be3fed5e35582f8c4129cd0929
Drew <Laosing@users.noreply.github.com>
2015-07-27 01:18:47 -0400

    Remove rvm code

--- os/ubuntu/install_applications.sh
+++ os/ubuntu/install_applications.sh
@@ -93,8 +93,8 @@ add_curl_software() {
 
     # RVM & Ruby
     [ $(cmd_exists "rvm") -eq 1 ] \
-        && gpg -q --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
-        && \curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 >/dev/null 2>&1
+        && gpg -q --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+        && \curl -sSL https://get.rvm.io | bash -s stable --ruby
 }
 
 add_source_list() {
