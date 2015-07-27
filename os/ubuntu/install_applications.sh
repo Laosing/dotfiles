50617636e3e7990788b50fbcbd5dbb30f8a75cb3
Drew <Laosing@users.noreply.github.com>
2015-07-27 01:33:54 -0400

    rvm fix

--- os/ubuntu/install_applications.sh
+++ os/ubuntu/install_applications.sh
@@ -95,6 +95,7 @@ add_curl_software() {
     [ $(cmd_exists "rvm") -eq 1 ] \
         && gpg -q --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
         && \curl -sSL https://get.rvm.io | bash -s stable --ruby
+        && rvm reset && rvm version
 }
 
 add_source_list() {
